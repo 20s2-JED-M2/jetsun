f4a4562c547618d13a6934130aa895dc07721439
184424G <184424G@CL1PC22.SEGSTUD.NYP.EDU.SG>
2021-01-22 10:40:24 +0800

    added package

---
+++ src/java/sg/edu/nyp/entities/Customer.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nyp.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 182281M
+ */
+public class Customer {
+    private String nricNo;
+    private String title;
+    private String name;
+    private String email;
+    private String homeAdd;
+    private String billingAdd;
+    private String passportNo;
+    private Date passportExpiry;
+    private String mobilePhone;
+    private Date dob;
+    private String officePhone;
+    private String homePhone;
+    private String KrisFlyer;
+    private String password;
+
+    public String getNricNo() {
+        return nricNo;
+    }
+
+    public void setNricNo(String nricNo) {
+        this.nricNo = nricNo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomeAdd() {
+        return homeAdd;
+    }
+
+    public void setHomeAdd(String homeAdd) {
+        this.homeAdd = homeAdd;
+    }
+
+    public String getBillingAdd() {
+        return billingAdd;
+    }
+
+    public void setBillingAdd(String billingAdd) {
+        this.billingAdd = billingAdd;
+    }
+
+    public String getPassportNo() {
+        return passportNo;
+    }
+
+    public void setPassportNo(String passportNo) {
+        this.passportNo = passportNo;
+    }
+
+    public Date getPassportExpiry() {
+        return passportExpiry;
+    }
+
+    public void setPassportExpiry(Date passportExpiry) {
+        this.passportExpiry = passportExpiry;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getOfficePhone() {
+        return officePhone;
+    }
+
+    public void setOfficePhone(String officePhone) {
+        this.officePhone = officePhone;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getKrisFlyer() {
+        return KrisFlyer;
+    }
+
+    public void setKrisFlyer(String KrisFlyer) {
+        this.KrisFlyer = KrisFlyer;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
